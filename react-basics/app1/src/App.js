bdeb8c8cbc2a85868123443e71f2e62ce2e4db25
Ira <iraupadhyay53@gmail.com>
2022-07-05 17:00:01 +0530

    react introduction

---
+++ react-basics/app1/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import OrderForm from './orderform';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
+      <OrderForm name='Ramesh'></OrderForm>
+      <OrderForm name='Pariwesh'></OrderForm>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-basics/app1/src/orderform.js
@@ -0,0 +1,13 @@
+
+
+
+ function OrderForm(props) {
+    return (
+        <div>
+            <h1>order form</h1>
+            <input value={props.name}></input>
+        </div>
+    )
+}
+
+export default OrderForm;
