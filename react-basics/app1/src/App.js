67e1191e661132ec615c1a9b3ba7e3243a5979e7
Ira <iraupadhyay53@gmail.com>
2022-07-06 10:28:41 +0530

    2 way data biding using usestate() hook

--- react-basics/app1/src/App.js
+++ react-basics/app1/src/App.js
@@ -2,14 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import OrderForm from './orderform';
 
-function App() {
+function App() {//contructor function
   return (
     <div className="App">
       <header className="App-header">
       
       </header>
       <OrderForm name='Ramesh'></OrderForm>
-      <OrderForm name='Pariwesh'></OrderForm>
     </div>
   );
 }
--- react-basics/app1/src/orderform.js
+++ react-basics/app1/src/orderform.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 
 
 
- function OrderForm(props) {
+function OrderForm(props) {
+    const [order, setOrder] = useState({
+        item: 'Laptop'
+    });
+    function updateValue(event) {
+        console.log(event.target.value);
+        setOrder({item:event.target.value});
+    }
     return (
         <div>
             <h1>order form</h1>
-            <input value={props.name}></input>
+            <input value={order.item} onChange={updateValue}></input>
+            <div>
+                <h1>{props.name}</h1>
+            </div>
         </div>
     )
 }
