caa1c8874bd1f6a4b7c4a911e983fdd652f63716
Ira <iraupadhyay53@gmail.com>
2022-07-22 11:09:52 +0530

    redux store using useSelector()

--- day-14/redux-demo/src/App.js
+++ day-14/redux-demo/src/App.js
@@ -1,12 +1,39 @@
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './App.css';
-import LogState from './Counter';
+import Counter, { ADD_TODO, DECREMENT, INCREMENT } from './Counter';
 
+//reducer
+function reducer(state = { counter: 10 }, action) {
+  //global logic
+  switch (action.type) {
+    case ADD_TODO:
+      return { ...state, datafromComponent: action.payload }
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+//store
+var store = createStore(reducer, enableDevTools());
 function App() {
   return (
     <div>
-      <LogState/>
+      <Provider store={store}>
+        <Counter />
+      </Provider>
     </div>
   );
 }
 
+
+function enableDevTools() {
+  return (
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+}
 export default App;
--- day-14/redux-demo/src/Counter.js
+++ day-14/redux-demo/src/Counter.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { createStore } from "redux";
+
 //action types
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
-const ADD_TODO = 'ADD_TODO';
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const ADD_TODO = 'ADD_TODO';
 //action creators
 function increment(name) {
     return { //action 
@@ -14,42 +16,15 @@ function increment(name) {
 function decrement() {
     return { type: DECREMENT };
 }
-//reducer
-function reducer(state = { counter: 10 }, action) {
-    //global logic
-    switch (action.type) {
-        case ADD_TODO:
-            return { ...state, datafromComponent: action.payload }
-        case INCREMENT:
-            return state + 1;
-        case DECREMENT:
-            return state - 1;
-        default:
-            return state;
-    }
-}
-
-//store
-var store = createStore(reducer, enableDevTools());
-
-
-export default function LogState() {//component
-    // const [name, setName] = useState();//local state  //TODO
+export default function Counter() {
+    const reduxState = useSelector((state) => state);
+    const dispatch = useDispatch();
     return (
         <div>
-            <button onClick={(event) => store.dispatch(increment('Pariwesh'))}>Increment</button>
-            <button onClick={() => store.dispatch(decrement())}> Decrement</button>
+            <div>Count: {reduxState.counter}</div>
         </div>
-    )
-}
-
-store.subscribe(LogState);//register
-
-function enableDevTools() {
-    return (
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 }
-// store.dispatch({ type: '' });
-// store.dispatch(increment());
-// store.dispatch(decrement());
+
+
+
